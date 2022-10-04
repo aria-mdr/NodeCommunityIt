1e1a14474bfb87d5c97e448c744e10c2deb81604
Aria Moradpour <ariamoradpour.am@gmail.com>
2022-10-03 20:02:43 -0400

    feat: add login page

--- app.js
+++ app.js
@@ -11,9 +11,15 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/client/index.html'));
 })
 
+app.get('/login', (req, res) => {
+    console.log('accessing route /login, METHOD = GET');
+    res.sendFile(path.join(__dirname, '/client/login.html'));
+})
+
 
 app.listen(port, () => {
     console.log('Listening on port: ' + port);
 })
 
+// exporting app so vercel can access it
 module.exports = app;
